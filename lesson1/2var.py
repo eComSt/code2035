af6fdd7954df6f50f694912ba874eb94c34f89c6
pattycha <pattycha@student.21-school.ru>
2022-12-22 20:30:39 +0700

    1 lesson

---
+++ lesson1/2var.py
@@ -0,0 +1,4 @@
+likes_number = 1000000
+group_name = 'Мемы про котиков'
+username = input('Представьтесь, пожалуйста!\n')
+print('Привет, ' + username)
---
+++ lesson1/4calc.py
@@ -0,0 +1,8 @@
+number_1 = int(input()) 
+number_2 = int(input())
+
+summ = number_1 + number_2 
+diff = number_1 - number_2
+
+print('Сумма: ' + str(summ)) 
+print('Разность: ' + str(diff)) 
