5ca53aa98071cf5981533d5e5df21917fca34f50
pattycha <pattycha@student.21-school.ru>
2022-12-27 19:59:19 +0700

    lesson code

--- lesson1/2var.py
+++ lesson1/2var.py
@@ -1,4 +1,3 @@
-likes_number = 1000000
-group_name = 'Мемы про котиков'
-username = input('Представьтесь, пожалуйста!\n')
-print('Привет, ' + username)
+num1="волк"
+print(f"{num1} спит")
+
--- lesson1/4calc.py
+++ lesson1/4calc.py
@@ -3,6 +3,7 @@ number_2 = int(input())
 
 summ = number_1 + number_2 
 diff = number_1 - number_2
-
+mul = number_1*number_2
 print('Сумма: ' + str(summ)) 
 print('Разность: ' + str(diff)) 
+print('Произведение: ' + str(mul)) 
