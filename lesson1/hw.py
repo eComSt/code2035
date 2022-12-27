279e15952a76cc584600c2573b80311f4fcd19e9
pattycha <pattycha@student.21-school.ru>
2022-12-27 21:17:16 +0700

    homework

---
+++ lesson1/hw.py
@@ -0,0 +1,16 @@
+first_animal="Заяц"
+second_animal="Черепаха"
+#print(first_animal+"спит, "+second_animal+" идет")
+print("{} спит, {} идет".format(first_animal,second_animal))
+
+
+number_1=int(input())
+number_2=int(input())
+summ=number_1+number_2
+diff=number_1-number_2
+mul=number_1*number_2
+div=number_1/number_2
+print(f"Сумма:{summ}")
+print(f"Разность:{diff}")
+print(f"Произведение:{mul}")
+print(f"Отношение:{div}")
