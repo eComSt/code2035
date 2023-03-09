c3247a5bf78add7e3ce62baa4da8b10e0f2265bd
pattycha <pattycha@student.21-school.ru>
2023-03-09 22:10:21 +0700

    123

--- tanks.py
+++ tanks.py
@@ -21,4 +21,20 @@ class Tank:
         self.mes = f"«{self.model}» имеет лобовую броню «{self.armor}»"\
                     f"мм. при «{self.health}» ед. здоровья и урон в "\
                     f"«{self.damage}» единиц"
-    
+    def shot(self,some_tank:Tank)->None:
+        some_tank.health_down(self.damage)
+        if some_tank.health<=0:
+            self.mes = f"Экипаж танка «{some_tank.model}» уничтожен"
+        else:
+            self.mes = f"«{self.model}»: Точно в цель, у противника "\
+                        f"«{some_tank.model}» осталось «{some_tank.health}»"\
+                        f"единиц здоровья"
+
+    def helth_down(self,damage_taken:int)->None:
+        self.health-=damage_taken/self.armor
+        if self.health>0:
+            self.mes = f"«{self.model}»: Командир, по экипажу «{self.model}» "\
+                        f"попали, у нас осталось «{self.health}» очков здоровья"
+        else:
+            self.mes = f"«{self.model}»: Командир, по экипажу «{self.model}» "\
+            f"попали, у нас не осталось очков здоровья"
