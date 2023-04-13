9287920045b1f37a3a55a3dd302a1a309c9ba901
pattycha <pattycha@student.21-school.ru>
2023-04-13 21:55:09 +0700

    123

--- wiki.py
+++
@@ -1,8 +0,0 @@
-import wikipedia
-
-def get_wiki_article(article):
-    wikipedia.set_lang("ru")
-    try:
-        return f"{wikipedia.summary(article)}"
-    except:
-        return f"Не удалось найти информацию по теме {article}"
