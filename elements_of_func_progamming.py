01914aeb37211a10d07be494e60c573689bb2845
pattycha <pattycha@student.21-school.ru>
2023-05-11 22:52:09 +0700

    123

--- elements_of_func_progamming.py
+++ elements_of_func_progamming.py
@@ -47,41 +47,43 @@ good_of_eldorado = [
         "brand":"Siemens",
     }
 ]
+if __name__=="__main__":
+    test_list = [2,4,3,5,7,4,1,6,8,0,3,3,2]
+    x = sorted(good_of_eldorado, key = lambda item:item.get("price") if "price" in item.keys() else 0)
+    pprint(x)
 
-test_list = [2,4,3,5,7,4,1,6,8,0,3,3,2]
-x = sorted(good_of_eldorado, key = lambda item:item.get("price") if "price" in item.keys() else 0)
-pprint(x)
+    apple_list = filter(lambda item:item.get("price")>500_000 if "price" in item.keys() else False,good_of_eldorado)
+    print(isinstance(apple_list, Iterator))
+    pprint(list(apple_list))
 
-apple_list = filter(lambda item:item.get("price")>500_000 if "price" in item.keys() else False,good_of_eldorado)
-print(isinstance(apple_list, Iterator))
-pprint(list(apple_list))
+    test_list_string = map(lambda x:x*x, test_list)
+    pprint(list(test_list_string))
 
-test_list_string = map(lambda x:x*x, test_list)
-pprint(list(test_list_string))
+    names_list = ["Илья","Игорь","Семен","Владислав"]
+    surnames_list = ["Коробкин", "Петардов","Зелепупкин","Петренко"]
+    patronymic_list = ["Игоревич","Семеныч","Владиславович","Ильич"]
 
-names_list = ["Илья","Игорь","Семен","Владислав"]
-surnames_list = ["Коробкин", "Петардов","Зелепупкин","Петренко"]
-patronymic_list = ["Ильbx","Игорь","Семен","Владислав"]
+    new_list = [f"{names_list[i]} {surnames_list[i]}" for i in range(len(names_list))]
+    new_list2 = map(lambda name,surname:f"{name} {surname}",names_list,surnames_list)
+    print(list(new_list2))
 
-new_list = [f"{names_list[i]} {surnames_list[i]}" for i in range(len(names_list))]
-new_list2 = map(lambda name,surname:f"{name} {surname}",names_list,surnames_list)
-print(list(new_list2))
+    for num,i in enumerate(good_of_eldorado):
+        print(num+1,i["name"],i["brand"],i["price"] if "price" in i.keys() else 0)
 
-for num,i in enumerate(good_of_eldorado):
-    print(num+1,i["name"],i["brand"],i["price"] if "price" in i.keys() else 0)
+    print("__________")
 
-print("__________")
+    num = 0
+    for i in good_of_eldorado:
+        print(num+1,i["name"],i["brand"],i["price"] if "price" in i.keys() else 0)
+        num = num+1 
 
-num = 0
-for i in good_of_eldorado:
-    print(num+1,i["name"],i["brand"],i["price"] if "price" in i.keys() else 0)
-    num = num+1 
-
-print("__________")
-for i in range(len(good_of_eldorado)):
-    print(i+1,good_of_eldorado[i]["name"],good_of_eldorado[i]["brand"],good_of_eldorado[i]["price"] if "price" in good_of_eldorado[i].keys() else 0)
+    print("__________")
+    for i in range(len(good_of_eldorado)):
+        print(i+1,good_of_eldorado[i]["name"],good_of_eldorado[i]["brand"],good_of_eldorado[i]["price"] if "price" in good_of_eldorado[i].keys() else 0)
 
 
-x = zip(names_list,surnames_list,test_list,)
+    for name, surname,patronymic in zip(names_list,patronymic_list,surnames_list):
+        print(name,surname,patronymic)
 
-print(list(x))
+
+    print(__name__)
---
+++ new.py
@@ -0,0 +1,8 @@
+import elements_of_func_progamming
+from pprint import pprint
+pprint(elements_of_func_progamming.good_of_eldorado)
+print(__name__)
+
+x = ["asdfsa","sdf",'dfghdfgh']
+x = list(map(str.capitalize,x))
+print(x)
