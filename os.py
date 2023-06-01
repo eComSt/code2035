c3b85b1a42c1c72f55ca1953e5c7911bbde9ce21
pattycha <pattycha@student.21-school.ru>
2023-06-01 21:58:01 +0700

    123

---
+++ os.py
@@ -0,0 +1,16 @@
+import os
+current_path = os.path.dirname(os.path.abspath(__file__))
+parent_path = os.path.dirname(current_path)
+print(current_path)
+print(parent_path)
+def get_all_files(path):
+    try:
+        for i in os.listdir(path):
+            new = os.path.join(path,i)
+            if os.path.isdir(new):
+                print(f"Folder {new}")
+                get_all_files(new)
+            else:
+                print(f" -{new}")
+    except:pass
+get_all_files(parent_path)
