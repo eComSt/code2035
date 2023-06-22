b0f439439af150b0491cdb0260e3c3e524f73e39
eComS <eComS@live.com>
2023-06-22 21:25:53 +0700

    12312

--- 123.py
+++ 123.py
@@ -13,3 +13,20 @@ def str_counter(data):
         print(f"{i}:{counter}")
 str_counter(strg)
 print(c)
+def strcounter(s): # решение за N
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+    for sym, count in syms_counter.items():
+        print(sym, count)
+strcounter('aabbbbccd')
+
+word = "Hello World"
+letter_count = {}
+for letter in word:
+    if letter in letter_count:
+        letter_count[letter] += 1
+    else:
+        letter_count[letter] = 1
+for letter, count in letter_count.items():
+    print(f"{letter}: {count}")
