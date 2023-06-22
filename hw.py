cb5b5ae2860042c7f2c4786da2cf5559f59f2f6b
eComS <eComS@live.com>
2023-06-22 21:35:15 +0700

    123

--- hw.py
+++ hw.py
@@ -1,2 +1,13 @@
 def is_palindrome(s):
     return s == s[::-1]
+
+
+def is_palindrome2(s):
+    l = len(s)
+    for i in range(l//2):
+        if s[i] != s[l - i - 1]:
+            return False
+    return True
+
+
+print( is_palindrome2("helloolleho"))
