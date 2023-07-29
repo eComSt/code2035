938ebc56a081751830362f2141b3a0255d3ea1aa
eComS <eComS@live.com>
2023-07-29 21:35:13 +0700

    123

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from .models import Advertisement
 # Register your models here.
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id','title', 'description', 'price','created_date', 'auction']
+    list_filter = ['auction','created_at']
+    actions = ['make_auction_false','make_auction_true']
+    fieldsets = (
+        ("Общие", {
+            'fields': ('title', 'description')
+        }),
+         ("Финансы", {
+            'fields': ('price', 'auction'),
+            'classes': ('collapse',),
+        })
+    )
+    @admin.action(description="Убрать возможность торгов")
+    def make_auction_false(self, request, queryset):
+        queryset.update(auction=False)
+        
+    @admin.action(description="Вернуть возможность торгов")
+    def make_auction_true(self, request, queryset):
+        queryset.update(auction=True)
+
+admin.site.register(Advertisement, AdvertisementAdmin)
--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.html import format_html
+from django.contrib import admin
 
 class Advertisement(models.Model):
     title = models.CharField("Заголовок",max_length=128)
@@ -8,3 +10,19 @@ class Advertisement(models.Model):
     auction = models.BooleanField("Торг",help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField("Дата создания",auto_now_add=True)
     updated_at = models.DateTimeField("Дата обновления",auto_now=True)
+    
+    @admin.display(description = "дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>',created_time)
+        else:
+            return self.created_at.strftime("%d.%m.%Y %H:%M:%S")
+
+
+    def __str__(self):
+        return f"id = {self.title},title = {self.title}, price = {self.price}"
+
+    class Meta:
+        db_table = "advertisements"
