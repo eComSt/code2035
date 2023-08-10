6189f9293169521e939998ec06ffa59ca9b9213e
eComS <eComS@live.com>
2023-08-10 21:09:51 +0700

    213

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -11,18 +11,27 @@ class Advertisement(models.Model):
     created_at = models.DateTimeField("Дата создания",auto_now_add=True)
     updated_at = models.DateTimeField("Дата обновления",auto_now=True)
     
-    @admin.display(description = "дата создания")
-    def created_date(self):
-        from django.utils import timezone
-        if self.created_at.date() == timezone.now().date():
-            created_time = self.created_at.time().strftime("%H:%M:%S")
-            return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>',created_time)
-        else:
-            return self.created_at.strftime("%d.%m.%Y %H:%M:%S")
 
 
-    def __str__(self):
-        return f"id = {self.title},title = {self.title}, price = {self.price}"
 
-    class Meta:
-        db_table = "advertisements"
+
+
+
+
+
+    
+    # @admin.display(description = "дата создания")
+    # def created_date(self):
+    #     from django.utils import timezone
+    #     if self.created_at.date() == timezone.now().date():
+    #         created_time = self.created_at.time().strftime("%H:%M:%S")
+    #         return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>',created_time)
+    #     else:
+    #         return self.created_at.strftime("%d.%m.%Y %H:%M:%S")
+
+
+    # def __str__(self):
+    #     return f"id = {self.title},title = {self.title}, price = {self.price}"
+
+    # class Meta:
+    #     db_table = "advertisements"
---
+++ float_decimal.py
@@ -0,0 +1,3 @@
+a=1/3
+for _ in range (350):
+    print(a:=a/10)
