a4d5e408b14c6aa951333172fecc59583fae8dd1
eComS <eComS@live.com>
2023-08-17 22:40:16 +0700

    123

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from .models import Advertisement
 from .forms import AdvertisementForm
+
+
 def index(request):
     advertisements = Advertisement.objects.all()
-    context = {'advertisements':advertisements}
-    return render(request, 'index.html',context)
+    context = {'advertisements': advertisements}
+    return render(request, 'index.html', context)
+
 
 def top_sellers(request):
     return render(request, 'top-sellers.html')
 
+
 def advertisement_post(request):
-    form  = AdvertisementForm()
-    context = {'form':form}
-    return render(request, 'advertisement-post.html',context)
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisement(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    context = {'form': form}
+    return render(request, 'advertisement-post.html', context)
