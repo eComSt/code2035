8ec75fb57804ba7a25cefdb060d4084e8f439dcb
eComS <eComS@live.com>
2023-08-24 22:09:31 +0700

    new templates

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -7,11 +7,11 @@ from .forms import AdvertisementForm
 def index(request):
     advertisements = Advertisement.objects.all()
     context = {'advertisements': advertisements}
-    return render(request, 'index.html', context)
+    return render(request, 'app_advertisements/index.html', context)
 
 
 def top_sellers(request):
-    return render(request, 'top-sellers.html')
+    return render(request, 'app_advertisements/top-sellers.html')
 
 
 def advertisement_post(request):
@@ -26,4 +26,4 @@ def advertisement_post(request):
     else:
         form = AdvertisementForm()
     context = {'form': form}
-    return render(request, 'advertisement-post.html', context)
+    return render(request, 'app_advertisements/advertisement-post.html', context)
